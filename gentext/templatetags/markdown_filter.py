5e0786de94093efac1f300b87b39c9cc649049b0
tom <tom_dekeyser_7@hotmail.com>
2016-07-28 16:28:52 +0200

    markdown filter updated

--- gentext/templatetags/markdown_filter.py
+++ gentext/templatetags/markdown_filter.py
@@ -3,8 +3,11 @@ import markdown
 
 register = template.Library()
 
+
 @register.filter
 def markdownify(text):
-	# safe_mode governs how the function handles raw HTML + creates anchors to headers with table-of-contents extension
-	md = markdown.Markdown(extensions=['markdown.extensions.toc'], safe_mode='escape') 
-	return md.convert(text)
+    md = markdown.Markdown(
+        extensions=['markdown.extensions.toc'],
+        safe_mode='escape'
+    )
+    return md.convert(text)
