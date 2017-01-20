4e31295e92f11c07a0f4fd1b5003fff569d7077d
tom <tom_dekeyser_7@hotmail.com>
2017-01-20 17:57:25 +0100

    rm

--- static/JJDLP/js/smooth_anchorage.js
+++
@@ -1,6 +0,0 @@
-$ (document).ready(function(){
-	$('a[href*=#]').on('click', function(event){     
-	    event.preventDefault();
-	    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-	});
-});
